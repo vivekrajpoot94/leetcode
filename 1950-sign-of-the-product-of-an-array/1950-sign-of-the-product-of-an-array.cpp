b402dff407ce11b2bb4493e9fb5e4845e9eb79cb
vivekrajpoot94 <149244529+vivekrajpoot94@users.noreply.github.com>
2025-08-04 00:10:35 +0530

    Time: 0 ms (100%), Space: 13.7 MB (81.3%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) return 0;
+            if(nums[i]<0) count++;
+        }
+        return count%2 ? -1:1;
+        // if(count%2==0)
+        // return 1;
+        // else
+        // return -1;
+    }
+};
