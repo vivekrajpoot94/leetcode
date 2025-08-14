41f29581d2b7b1cecad32e30ac3d8dbb5554391a
vivekrajpoot94 <149244529+vivekrajpoot94@users.noreply.github.com>
2025-08-14 12:57:15 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.46%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -9,7 +9,7 @@ public:
             if(pre==0) pre=1;
             if(suff==0) suff=1;
             pre*=nums[i];
-            suff*=nums[n-1-i];
+            suff*=nums[n-i-1];
            maxi=max(maxi,max(pre,suff));
         }
    return maxi;
